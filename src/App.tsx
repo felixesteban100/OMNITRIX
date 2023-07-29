c80c39fa90489c49b36d0206429c10e730fa901f
Felix Monegro <89742789+felixesteban100@users.noreply.github.com>
2023-07-29 10:59:11 -0400

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+// import OmnitrixOriginal from "./components/OmnitrixOriginal"
+// import OmnitrixUltimate from "./components/OmnitrixUltimate"
+import OmnitrixOmniverse from "./components/OmnitrixOmniverse"
+
+function App() {
+  return (
+    <div>
+      {/* <Omnitrixes /> */}
+      {/* <OmnitrixOriginal/> */}
+      {/* <OmnitrixUltimate /> */}
+      <OmnitrixOmniverse/>
+    </div>
+  )
+}
+
+
+export default App
